6e9c1842f4596fb7934ba67eac2a18cd87273f9f
lmodenbach <lmodenbach@gmail.com>
2021-06-24 13:48:37 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,58 @@
+//read in json data from samples.json
 d3.json("./static/data/samples.json").then((importedData) => {
-    console.log(importedData);
-    var data = importedData;
-});  
-    /* // Sort the data array using the greekSearchResults value
-    data.sort(function(a, b) {
-      return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-    });
+    var data = importedData; 
+});
+
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-    // Slice the first 10 objects for plotting
-    data = data.slice(0, 10);
+//     // Slice the first 10 objects for plotting
+//     data = data.slice(0, 10);
   
-    // Reverse the array due to Plotly's defaults
-    data = data.reverse();
+//     // Reverse the array due to Plotly's defaults
+//     data = data.reverse();
   
-    // Trace1 for the Greek Data
-    var trace1 = {
-      x: data.map(row => row.greekSearchResults),
-      y: data.map(row => row.greekName),
-      text: data.map(row => row.greekName),
-      name: "Greek",
-      type: "bar",
-      orientation: "h"
-    };
+//     // Trace1 for the Greek Data
+//     var trace1 = {
+//       x: data.map(row => row.greekSearchResults),
+//       y: data.map(row => row.greekName),
+//       text: data.map(row => row.greekName),
+//       name: "Greek",
+//       type: "bar",
+//       orientation: "h"
+//     };
   
-    // data
-    var chartData = [trace1];
+//     // data
+//     var chartData = [trace1];
   
-    // Apply the group bar mode to the layout
-    var layout = {
-      title: "Greek gods search results",
-      margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-      }
-    };
+//     // Apply the group bar mode to the layout
+//     var layout = {
+//       title: "Greek gods search results",
+//       margin: {
+//         l: 100,
+//         r: 100,
+//         t: 100,
+//         b: 100
+//       }
+//     };
   
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("plot", chartData, layout);
-  }); */
+//     // Render the plot to the div tag with id "plot"
+//     Plotly.newPlot("plot", chartData, layout);
+//   });
